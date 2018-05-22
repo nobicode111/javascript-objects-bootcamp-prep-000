c165bec2c8a89e4e67cb80147130e58322ec640a
tram nguyen <tramnguyendalat1989@yahoo.com>
2018-05-22 00:49:24 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,6 +4,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
   return playlist
 }
 function removeFromPlaylist(playlist, artistName) {
-  return delete playlist.artistName;
+  delete playlist.artistName;
   
 }
